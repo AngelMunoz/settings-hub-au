779af57c3a3587bcd8d27141841c9f0585cd4abd
Angel Daniel Munoz Gonzalez <scythes.lol@hotmail.com>
2018-09-08 22:58:47 -0500

    remove dist dir

--- dist/tun-settings-hub/0ehntagj.js
+++
@@ -1,2 +0,0 @@
-/*! Built with http://stenciljs.com */
-const{h:t}=window.TunSettingsHub;class e{render(){return t("h1",null,"404")}static get is(){return"not-found"}static get style(){return""}}export{e as NotFound};
